7be1725b3581efea733d995b82519e764e7d3e65
marcospaulorr <marcospauloricartereboucas@gmail.com>
2025-03-10 14:45:20 -0300

    deepqlearning

--- train_mountaincar_deepqlearning.py
+++ train_mountaincar_deepqlearning.py
@@ -9,6 +9,9 @@ import gc
 import argparse
 from datetime import datetime
 from collections import deque
-from keras import Sequencial
+from keras import Sequential
 from keras.layers import Dense
 from keras.activations import relu, linear
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+from DeepQLearning import DeepQLearning
